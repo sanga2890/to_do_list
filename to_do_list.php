b920d5d76c859f0299b2251eae92a837cd011219
Stefano <stefano2890@gmail.com>
2020-11-12 21:42:04 +0100

    added select

--- to_do_list.php
+++ to_do_list.php
@@ -1,11 +1,35 @@
 <?php
 include 'list.php';
+echo "<form method='post'>";
+echo "<select name='Actions'>";
+echo "<option value ='all'>";
+echo "All";
+echo "</option>";
+echo "<option value ='true'>";
+echo "Complete";
+echo "</option>";
+echo "<option value = ";
+echo false;
+echo ">";
+echo "To Do";
+echo "</option>";
+echo "</select>";
+echo "<input type='submit' name='submit' value='Submit'>";
+echo "</form>";
+
+if(isset($_POST['submit'])){
+   if(!empty($_POST['Actions'])) {
+       $selected = $_POST['Actions'];
+        var_dump($selected);
+       }
+   }
 
 $status = false;
+var_dump($status);
 $field = 'priority';
 $filter = [];
 foreach ($list as $originalKey => $item) {
-  if ($status === 'all' || $item['complete'] == $status) {
+  if ($selected === 'all' || $item['complete'] == $selected) {
     if (isset($field) && isset($item[$field])){
     $filter[$originalKey] = $item[$field];
   } else {
